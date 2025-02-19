dd7b07779a093ca9b50b29ce1f5b0cb466cbbeb5
Matney0 <mlmoller21@amarillocollege.com>
2025-02-19 10:59:28 -0600

    Update Program.cs
    added comments

--- Week5.3/Program.cs
+++ Week5.3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -20,6 +20,7 @@ namespace Week5._3
         {
             //Program to simulate a Call Center menu
 
+            //Options for the user 
             Console.WriteLine("Welcome to the Call Center!");
             Console.WriteLine("Please select an option from the menu below:");
             Console.WriteLine("1. Add customer to the collection");
@@ -27,48 +28,62 @@ namespace Week5._3
             Console.WriteLine("3. Display all customers in the queue");
             Console.WriteLine("4. Exit the program");
 
+            //reads user input and converts it for the computer to read
             string input = Console.ReadLine();
             int choice = Convert.ToInt32(input);
 
-
-            switch (choice) {
-                case 0: Console.WriteLine("Invalid choice. Please try again.");
+            //Switch clause for program and its options 
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Invalid choice. Please try again.");
                     break;
-                case 1: AddCustomer();
+                case 1:
+                    AddCustomer();
                     break;
-                case 2: ServeCustomer();
+                case 2:
+                    ServeCustomer();
                     break;
-                case 3: DisplayCustomers();
+                case 3:
+                    DisplayCustomers();
                     break;
-                case 4: ExitProgram();
+                case 4:
+                    ExitProgram();
                     Environment.Exit(0);
                     break;
 
             }
         }
 
-
-            public static void AddCustomer()
+        //Public method for adding a customer
+        public static void AddCustomer()
         {
+            //Puts users information into a list 
             List<string> customers = new List<string>();
             int i = 1;
+            //Stores name in the customer list 
             Console.WriteLine("Please enter the customer's name:");
             //adding new customer to the list
             string name = Console.ReadLine();
             customers.Add(name);
+            //Other usually important information, though it doesnt store this user information 
             Console.WriteLine("Please enter the customer's phone number:");
             string phone = Console.ReadLine();
             Console.WriteLine("Please enter the customer's email address:");
             string email = Console.ReadLine();
             Console.WriteLine("You will reacieve a queue number shortly.");
+            //Provides a queue number and increments for every customer added 
             Console.WriteLine("Your queue number is: " + i);
             i++;
 
         }
+        //Public method for serving the customer in queue
         public static void ServeCustomer()
-        {   
+        {
+            //Simple output to users 
             Console.WriteLine("Serving the next customer in the queue... ");
         }
+        //Method to display customers waiting in queue
         public static void DisplayCustomers()
         {
             Console.WriteLine("Displaying all customers in the queue...");
@@ -76,6 +91,7 @@ namespace Week5._3
             List<string> customers = new List<string>();
 
         }
+        //Method for leaving the program 
         public static void ExitProgram()
         {
             Console.WriteLine("Exiting the program...");
